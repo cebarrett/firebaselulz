58a4ea4c13f78d2b7c1bb3490469350fac6a2a03
Chris Barrett <chris.barrett@opower.com>
2014-09-15 14:50:33 -0700

    plot w/ elroi. graph doesnt look right

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -3,8 +3,19 @@
     var firebase = new Firebase("https://blazing-heat-948.firebaseio.com/test");
     var data = {};
     firebase.on('value', function(snapshot) {
-        data = snapshot.val();
-        $('#graph-container').text(JSON.stringify(data));
+        data = convertToElroiSeries(snapshot.val());
+        $('#graph-container').children().remove();
+        elroi($('#graph-container'), data);
     });
+
+    function convertToElroiSeries(data) {
+        series = [];
+        for (timestamp in data) {
+            date = new Date(Number.parseInt(timestamp)).toISOString();
+            value = data[timestamp];
+            series.push({"date": date, "value": value});
+        }
+        return series;
+    }
 })();
 
