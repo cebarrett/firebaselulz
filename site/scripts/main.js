f684301058826655f4f57b52784334818af03ee4
Chris Barrett <chris.barrett@opower.com>
2014-09-15 12:06:34 -0700

    wired up to firebase; no graph yet

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -1,4 +1,10 @@
 (function() {
-    var graph = elroi($('#container'), [4,6,8,4,3,9]);
+//    var graph = elroi($('#container'), [4,6,8,4,3,9]);
+    var firebase = new Firebase("https://blazing-heat-948.firebaseio.com/test");
+    var data = {};
+    firebase.on('value', function(snapshot) {
+        data = snapshot.val();
+        $('#graph-container').text(JSON.stringify(data));
+    });
 })();
 
